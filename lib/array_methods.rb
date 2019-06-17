7cfe49360df84b659837581b10d4881d69734b98
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-17 01:58:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- #sort
+  #sort
 end
 
 def using_reverse(array)
-#reverse
+  #reverse
 end
 
 def using_first(array)
- #first
+  #first
 end
 
 def using_last(array)
- #last
+  #last
 end
 
 def using_size(array)
-#size
+  #size
 end
